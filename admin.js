5799238e0b33d95430473baabfde812dcef1f497
Halfdan <halfdansc@gmail.com>
2025-04-11 11:07:10 +0200

    fixed admin dashboard access issues

--- admin.js
+++ admin.js
@@ -1,9 +1,24 @@
+import { secure } from "./secure.js";
+
 const goIndexButton = document.getElementById("goIndex");
 const goHomeButton = document.getElementById("goHome");
 const logoutButton = document.getElementById("logout");
 
 const checkAuth = async () => {
+    try {
+        const response = await secure();
+        const data = await response.json();
+        if (response.ok) {
+            if (!data.role.includes("Admin")) {
+                logout();
+            }
+        } else {
+            logout();
+        }
 
+    } catch (error) {
+        console.error("Access denied");
+    }
 };
 
 const goToIndex = () => {
@@ -14,6 +29,22 @@ const goToHome = () => {
     window.location.href = "home.html";
 };
 
+const logout = async () => {
+    const response = await fetch("http://localhost:5141/api/auth/logout", {
+        method: "POST",
+        credentials: "include"
+    });
+
+    if (response.ok) {
+        goToIndex();
+    } else {
+        console.error("Logout failed.");
+    }
+};
+
+// run at start
+checkAuth();
+
 // event handlers
 goIndexButton.addEventListener("click", event => {
     goToIndex();
@@ -21,5 +52,9 @@ goIndexButton.addEventListener("click", event => {
 
 goHomeButton.addEventListener("click", event => {
     goToHome();
+});
+
+logoutButton.addEventListener("click", event => {
+    logout();
 });
 
--- home.js
+++ home.js
@@ -1,6 +1,9 @@
+import { secure } from "/secure.js";
+
 // dom elements
 const logoutButton = document.getElementById("logout");
 const welcomeTitle = document.getElementById("welcome");
+const adminButton = document.getElementById("adminDashboard");
 
 const checkAuth = async () => {
     const response = await fetch("http://localhost:5141/api/auth/profile", {
@@ -37,8 +40,28 @@ const logout = async () => {
     }
 };
 
+const buttonIfAdmin = async () => {
+    try {
+        const response = await secure();
+
+        if (response.ok) {
+            const data = await response.json();
+            
+            if (Array.isArray(data.role) && data.role.includes("Admin")) {
+                adminButton.addEventListener("click", () => {
+                    window.location.href = "AdminDashboard.html";
+                });
+                adminButton.style.display = "block";
+            }
+        }
+    } catch (error) {
+        console.log(":)")
+    }
+};
+
 // run at start
 checkAuth();
+buttonIfAdmin();
 
 // event handlers
 logoutButton.addEventListener("click", event => {
